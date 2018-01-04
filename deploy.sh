344b94902e5d5e5a960347463b01365625689b4f
Rodrigo Floriano de Souza <florianocomercial@gmail.com>
2018-01-04 17:27:23 -0200

    em desenvolvimento

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+ENV=$1
+PRIVATE_SSH_KEY=$2
 ANSIBLE_HOST_KEY_CHECKING=False
 
-ansible-playbook -i hosts --private-key=rodrigofloriano-keyus.pem site.yml 
+if [[ -z "$ENV" || -z "$PRIVATE_SSH_KEY" ]]; then
+	echo "Please enter the parameter for the environment:"
+	echo ""
+	echo "$0 <env> <path/private/ssh/key>"
+	echo ""
+	echo "Exemples:"	
+	echo ""
+	echo "	Staging 	= $0 stg ~/.ssh/id_rsa"
+	echo "	Production 	= $0 prd ~/.ssh/id_rsa"
+	echo "	AWS	 	= $0 aws ~/.ssh/id_rsa"
+	echo ""
+	exit 1
+fi
+
+ansible-playbook -i inventories/${ENV}/hosts --private-key=${PRIVATE_SSH_KEY} site.yml
